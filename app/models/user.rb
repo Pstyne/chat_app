3878f9f336eeb2034f04b08dd38523853d637530
Andrew Birt <abirt1986@gmail.com>
2020-10-27 01:08:31 -0400

    generate message model, populate chatroom with message data

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3, maximum: 30 }
   validates_uniqueness_of :username, case_sensitive: false
   has_secure_password
+  has_many :messages
 end
