1f188ab7e0c1d1bb832d7b4939500040ad62ee11
Andrew Birt <abirt1986@gmail.com>
2020-10-27 03:32:21 -0400

    add validation messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Account created successfully"
+      flash[:success] = "Account created successfully"
       session[:user_id] = @user.id
       redirect_to root_path
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 3, maximum: 30 }
-  validates_uniqueness_of :username, case_sensitive: false
   has_secure_password
+  validates :username, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :password, presence: true, length: { minimum: 3, maximum: 30 }, confirmation: true
+  validates_uniqueness_of :username, case_sensitive: false
   has_many :messages
 end
